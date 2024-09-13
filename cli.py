3898734b9d880b27e8e2d807b93b2b47f41084d9
TheRealHedgeLord <therealhedgelord@gmail.com>
2024-09-14 01:50:04 +0800

    cli helper

--- cli.py
+++ cli.py
@@ -4,11 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from modules import help
 from lambda_function import lambda_handler
 
 if __name__ == "__main__":
     argv = sys.argv
-    event = {"module": argv[1], "method": argv[2], "args": list(argv[3::])}
-    response = lambda_handler(event, None)
-    if response["statusCode"] != 200:
-        raise Exception(str(response))
+    if argv[1] == "help":
+        help()
+    else:
+        event = {"module": argv[1], "method": argv[2], "args": list(argv[3::])}
+        response = lambda_handler(event, None)
+        if response["statusCode"] != 200:
+            raise Exception(str(response))
--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,7 @@
+import inspect
+
+from typing import Callable
+
 from modules.portfolio import PortfolioModule
 from modules.scroll import ScrollMarksTracker
 from modules.linea import LineaPointsTracker
@@ -9,3 +13,20 @@ MODULES = {
     "linea": LineaPointsTracker,
     "gmx": GMXPerformanceTracker,
 }
+
+
+def _get_method_annotation(method: Callable) -> str:
+    args = inspect.getargs(method.__code__).args
+    return "{}({})".format(
+        method.__name__, ", ".join([arg for arg in args if arg != "self"])
+    )
+
+
+def help() -> None:
+    for module in MODULES:
+        print(f"\033[1m{module}\033[0m")
+        for name in dir(MODULES[module]):
+            if name[0] != "_":
+                method = getattr(MODULES[module], name)
+                if hasattr(method, "__code__") and hasattr(method, "__name__"):
+                    print(f"\t{_get_method_annotation(method)}")
